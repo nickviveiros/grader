82f23ce64ea3e04a1089f87e92e1e32cc5727a85
nickviveiros <nickviveiros@me.com>
2014-06-13 07:15:49 -0400

    Initial Commit

---
+++ Grader/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Grader
+//
+//  Created by Nicholas Viveiros on 6/13/14.
+//  Copyright (c) 2014 Nicholas Viveiros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
